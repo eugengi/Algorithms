6a746482ea38deb0a4aafc423b2577c98638c6e2
apexdev <mwangi.em37@gmail.com>
2024-05-01 21:15:21 +0300

    test: Add integration test
    Add test for the HackerRank entry-point function.
    
    [Note]
    Prefer a TDD approach using a BDD style convention.
    
    [ABV]
    - TDD: Test-Driven Development
    - BDD: Behavior-Driven Development

--- easy/priyankaAndToys/test.py
+++ easy/priyankaAndToys/test.py
@@ -1,10 +1,6 @@
 import unittest
 
-# from main import (
-#     # function_example_one,
-#     # function_example_two,
-#     # function_example_three
-# )
+from main import toys
 
 
 # ---------------------------- Tests ---------------------------- #
@@ -16,8 +12,26 @@ Script: main.py
 """
 
 
-# See example sample tests in
-# .
-# |-- easy
-#   |-- question
-#       |-- test.py
+class IntegrationTestCase(unittest.TestCase):
+  """
+  Test case to test the behavior of HackerRank entry-point.
+  """
+
+  def setUp(self) -> None:
+    self.under_test = toys
+
+  def test_should_return_number_of_containers_to_ship_all_toys(self) -> None:
+    # Given
+    items: list[int] = [1, 2, 3, 21, 7, 12, 14, 21]
+    expected = 4
+
+    # When
+    actual = self.under_test(items)
+
+    # Then
+    self.assertIsInstance(actual, int)
+    self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+  unittest.main()
