20346cb0ffe3720c542146c9585e2937acaae8b7
apexdev <mwangi.em37@gmail.com>
2024-05-02 22:04:05 +0300

    feat: Add initial solution
    Implement algorithm steps defined in `algorithm.txt`.
    
    [Chore]
    Setup `main` to ensure script is runnable and testable.
    
    Co-authored-by: Ndeda Kaduki <ndedakaduki@users.noreply.github.com>

--- easy/priyankaAndToys/main.py
+++ easy/priyankaAndToys/main.py
@@ -6,17 +6,26 @@
     Implementation: Eugene (apexDev37) & Ndeda (ndedakaduki)
 """
 
-# This is main entry point function to complete
+# This is the main entry point function to complete
 # on the HackerRank platform. 
 # Rename as per specified
 
-def function_name():
-    pass
+def toys(shipping_items: list[int]) -> int:
+    sorted_shipping_items = sorted(shipping_items)
+
+    current_min: int = 0
+    containers: int = 0
+    for item in sorted_shipping_items:
+        if not current_min or item > current_min:
+            current_min = item + 4
+            containers += 1
+    return containers 
 
 
-def main() -> None:
-    # Define your actual argument data here
-    print('Meaningful message output: ', function_name())
+def main() -> int:
+    shipping_items: list[int] = [1, 2, 3, 4, 5, 10, 11, 12, 13]
+    number_of_containers = toys(shipping_items=shipping_items)
+    print(number_of_containers)
 
 
 if __name__ == "__main__":
