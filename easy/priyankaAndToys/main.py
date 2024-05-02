3dc9dc224de02a269aa8acad998b9d20bb38d0d4
apexdev <mwangi.em37@gmail.com>
2024-05-02 22:11:25 +0300

    refactor: Modify src with minor patches
    Refactor consists of the following patches:
    - Update type hints.
    - Truncate code wherever possible.

--- easy/priyankaAndToys/main.py
+++ easy/priyankaAndToys/main.py
@@ -11,18 +11,15 @@
 # Rename as per specified
 
 def toys(shipping_items: list[int]) -> int:
-    sorted_shipping_items = sorted(shipping_items)
-
-    current_min: int = 0
-    containers: int = 0
-    for item in sorted_shipping_items:
+    containers, current_min = 0, 0
+    for item in sorted(shipping_items):
         if not current_min or item > current_min:
             current_min = item + 4
             containers += 1
     return containers 
 
 
-def main() -> int:
+def main() -> None:
     shipping_items: list[int] = [1, 2, 3, 4, 5, 10, 11, 12, 13]
     number_of_containers = toys(shipping_items=shipping_items)
     print(number_of_containers)
