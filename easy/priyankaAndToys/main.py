bdb425afff7e077fb25fb9902fbbaa0328e70191
apexdev <mwangi.em37@gmail.com>
2024-05-03 21:39:51 +0300

    sytle: Format with tool (autopep8)
    Format src with formatter tool `autopep8` to enforce consistency.
    
    [Commands]
    ➜ autopep8 --in-place --ignore=E101,E111 -a  --aggressive --aggressive *.py

--- easy/priyankaAndToys/main.py
+++ easy/priyankaAndToys/main.py
@@ -1,29 +1,30 @@
 """
 || HackerRank
-    Problem: Priyanka and Toys 
+    Problem: Priyanka and Toys
     Level: Easy
     Author: amititkgp
     Implementation: Eugene (apexDev37) & Ndeda (ndedakaduki)
 """
 
 # This is the main entry point function to complete
-# on the HackerRank platform. 
+# on the HackerRank platform.
 # Rename as per specified
 
+
 def toys(shipping_items: list[int]) -> int:
-    containers, current_min = 0, 0
-    for item in sorted(shipping_items):
-        if not current_min or item > current_min:
-            current_min = item + 4
-            containers += 1
-    return containers 
+  containers, current_min = 0, 0
+  for item in sorted(shipping_items):
+    if not current_min or item > current_min:
+      current_min = item + 4
+      containers += 1
+  return containers
 
 
 def main() -> None:
-    shipping_items: list[int] = [1, 2, 3, 4, 5, 10, 11, 12, 13]
-    number_of_containers = toys(shipping_items=shipping_items)
-    print(number_of_containers)
+  shipping_items: list[int] = [1, 2, 3, 4, 5, 10, 11, 12, 13]
+  number_of_containers = toys(shipping_items=shipping_items)
+  print(number_of_containers)
 
 
 if __name__ == "__main__":
-    main()
+  main()
