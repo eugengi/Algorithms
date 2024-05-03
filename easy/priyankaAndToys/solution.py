b5ee013bc81c5475f1132801863380a4e49c78d9
apexdev <mwangi.em37@gmail.com>
2024-05-03 21:36:55 +0300

    chore: Add passing solution
    Copy solution that passes all test cases on the HackerRank platform.
    
    Co-authored-by: Ndeda Kaduki <ndedakaduki@users.noreply.github.com>

--- easy/priyankaAndToys/solution.py
+++ easy/priyankaAndToys/solution.py
@@ -5,8 +5,16 @@
     Author: amititkgp
     Implementation: Eugene (apexDev37) & Ndeda (ndedakaduki)
 
-    Status: PASSED - '# of test cases passed'
+    Status: SUCCESS - All test cases passed!
 """
 
 # Copy and paste all your functional code from
 # from the HackerRank platform.
+
+def toys(shipping_items):
+    containers, current_min = 0, 0
+    for item in sorted(shipping_items):
+        if not current_min or item > current_min:
+            current_min = item + 4
+            containers += 1
+    return containers
