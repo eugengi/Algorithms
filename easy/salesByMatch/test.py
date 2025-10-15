9a9f31365aabf472c386941831857f9839dcd328
apexdev <mwangi.em37@gmail.com>
2025-10-15 07:12:09 +0300

    test: Add unit and entry-point tests
    Use TDD to define algorithm specs to be implemented.

--- easy/salesByMatch/test.py
+++ easy/salesByMatch/test.py
@@ -7,6 +7,8 @@ import unittest
 
 from main import (
   sock_merchant,
+  get_pairs,
+  group_by_color,
 )
 
 
@@ -16,7 +18,29 @@ class FunctionalTestCase(unittest.TestCase):
   Note: You might want to skip these tests if your approach changes.
   """
 
-  ...
+  def test_should_group_unorganized_socks_by_color(self) -> None:
+    # Given
+    socks = [1, 2, 5, 4, 5, 2, 1, 1, 1]
+    expected = {1: 4, 2: 2, 4: 1, 5: 2}
+    
+    # When
+    actual = group_by_color(socks)
+    
+    # Then
+    self.assertIsInstance(actual, dict)
+    self.assertDictEqual(actual, expected)
+
+  def test_should_compute_pairs_for_organized_socks_grouped_by_color(self) -> None:
+    # Given
+    socks = {1: 4, 2: 2, 4: 1, 5: 2}
+    expected = (2, 1, 0, 1,)
+
+    # When
+    actual = get_pairs(socks)
+
+    # Then
+    self.assertIsInstance(actual, tuple)
+    self.assertEqual(actual, expected)
 
 
 class IntegrationTestCase(unittest.TestCase):
@@ -28,7 +52,16 @@ class IntegrationTestCase(unittest.TestCase):
     self.under_test = sock_merchant
 
   def test_should_return_number_of_sock_pairs_with_matching_colors(self) -> None:
-    ...
+    # Given    
+    socks = [1, 2, 5, 4, 5, 2, 1, 1, 1]
+    expected = 4
+    
+    # When
+    actual = self.under_test(..., socks)
+    
+    # Then
+    self.assertIsInstance(actual, int)
+    self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
