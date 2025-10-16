56462cbbba2a198ba487976e3be4ffe7a5ea0073
apexdev <mwangi.em37@gmail.com>
2025-10-16 07:24:06 +0300

    feat: Add problem solution
    - Add impl that passes all tests to main
    - Add solution that passes all HackerRank tests to solution

--- easy/salesByMatch/main.py
+++ easy/salesByMatch/main.py
@@ -9,16 +9,27 @@
 # This is main entry point function to complete
 # on the HackerRank platform. 
 # Rename as per specified
+def sock_merchant(count: int, socks: list[int]) -> int:
+  organized_socks = group_by_color(socks)
+  pairs = get_pairs(organized_socks)
+  return sum(pairs)
 
-def sock_merchant(socks: int, pile: list[int]) -> int:
-  return 0
+  
+def group_by_color(socks: list[int]) -> dict[int, int]:
+  colors = set(socks)
+  return {color: socks.count(color) for color in colors}
 
 
+def get_pairs(socks: dict[int, int]) -> tuple[int]:
+  pairs = [(matching_socks // 2) for color, matching_socks in socks.items()]
+  return tuple(pairs)
+
+  
 def main() -> None:
   # Define your actual argument data here
-  sock_pile = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+  socks = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 
-  result = sock_merchant(len(sock_pile), sock_pile)
+  result = sock_merchant(..., socks)
   print(f'Pairs of matching color socks: {result}')
 
 
--- easy/salesByMatch/solution.py
+++ easy/salesByMatch/solution.py
@@ -5,8 +5,23 @@
     Author: Shafaet
     Implementation: Eugene (apexDev37)
 
-    Status: PASSED - '#/# of test cases passed'
+    Status: 8/8 of test cases PASSED
 """
 
 # Copy and paste all your functional code from
 # from the HackerRank platform.
+
+def sock_merchant(count: int, socks: list[int]) -> int:
+  organized_socks = group_by_color(socks)
+  pairs = get_pairs(organized_socks)
+  return sum(pairs)
+
+  
+def group_by_color(socks: list[int]) -> dict[int, int]:
+  colors = set(socks)
+  return {color: socks.count(color) for color in colors}
+
+
+def get_pairs(socks: dict[int, int]) -> tuple[int]:
+  pairs = [(matching_socks // 2) for color, matching_socks in socks.items()]
+  return tuple(pairs)
