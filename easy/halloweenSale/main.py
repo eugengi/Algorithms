b347fa7eeae7d3408a7f8f9ec8560725b96cb7c8
Eugene Mwangi <mwangi.em37@gmail.com>
2025-11-06 07:28:03 +0300

    feat: Add challenge impl
    Add DECLARATIVE implementation to main script to run against tests.

--- easy/halloweenSale/main.py
+++ easy/halloweenSale/main.py
@@ -10,14 +10,58 @@
 # on the HackerRank platform. 
 # Rename as per specified
 
-def function_name():
-    pass
+def how_many_games(
+  starting_price: int,
+  discount_price: int,
+  minimum_price: int,
+  budget: int
+) -> int:
+  discount_prices = _get_discount_game_prices(starting_price, discount_price, minimum_price)
+  if sum(discount_prices) > budget:
+    return len(_get_prices_within_budget(discount_prices, budget))
+  return len(discount_prices) + _compute_minimum_price_games(discount_prices, minimum_price, budget)
+  
+
+def _get_discount_game_prices(
+  starting_price: int, 
+  discount_price: int, 
+  minimum_price: int
+) -> tuple[int, ...]:
+  prices = tuple(reversed(range(minimum_price, starting_price + 1)))
+  discount_prices = [prices[n] for n in range(0, len(prices), discount_price)]
+  return tuple(discount_prices)
 
 
+def _get_prices_within_budget(
+    prices: tuple[int, ...], 
+    budget: int
+) -> tuple[int, ...]:
+  counter, within_budget = 0, []
+  while amount := (sum(within_budget) + prices[counter]) <= budget:
+    within_budget.append(prices[counter])
+    counter += 1
+  return tuple(within_budget)
+
+
+def _compute_minimum_price_games(
+  discount_prices: tuple[int, ...],
+  minimum_price: int,
+  budget: int
+) -> int:
+  balance = budget - sum(discount_prices)
+  return balance // minimum_price
+
+  
 def main() -> None:
-    # Define your actual argument data here
-    print('Meaningful message output: ', function_name())
+  # Define your actual argument data here
+  result = how_many_games(
+    starting_price=20,
+    discount_price=3,
+    minimum_price=6,
+    budget=80,
+  )
+  print("How many games can you buy?", result)
 
 
 if __name__ == "__main__":
-    main()
+  main()
