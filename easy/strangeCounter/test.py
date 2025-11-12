f3808404075a18be32009959bf130e736df6dc03
Eugene Mwangi <mwangi.em37@gmail.com>
2025-11-12 07:13:42 +0300

    test: Add unit and entry-point tests
    Use TDD to define algorithm specs to be implemented.

--- easy/strangeCounter/test.py
+++ easy/strangeCounter/test.py
@@ -3,10 +3,13 @@ Tests for problem: Strange Counter
 Script: main.py
 """
 
+from itertools import islice
 import unittest
 
 from main import (
   strange_counter,
+  _generate_cycle_fv_sequence,
+  _compute_cycle_time,
 )
 
 
@@ -16,7 +19,31 @@ class FunctionalTestCase(unittest.TestCase):
   Note: You might want to skip these tests if your approach changes.
   """
 
-  ...
+  def test_should_generate_sequence_where_each_number_is_double_previous(self) -> None:
+    # Given
+    expected = (3, 6, 12, 24,)
+
+    # When
+    result = _generate_cycle_fv_sequence()
+    actual = tuple(islice(result, len(expected)))
+
+    # Then
+    self.assertTrue(hasattr(result, '__iter__'))
+    self.assertEqual(actual, expected)
+
+  def test_should_compute_first_and_last_time_for_a_cycle(self) -> None:
+    # Given
+    first_value = 24
+    expected = (22, 45,)
+
+    # When
+    actual = _compute_cycle_time(first_value)
+
+    # Then
+    self.assertIsInstance(actual, tuple)
+    self.assertEqual(len(actual), 2)
+    self.assertEqual(actual, expected)
+
 
 
 class IntegrationTestCase(unittest.TestCase):
@@ -28,7 +55,16 @@ class IntegrationTestCase(unittest.TestCase):
     self.under_test = strange_counter
 
   def test_should_display_counter_value_for_given_time(self) -> None:
-    ...
+    # Given
+    time = 57
+    expected = 37
+
+    # When
+    actual = self.under_test(time)
+
+    # Then
+    self.assertIsInstance(actual, int)
+    self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
