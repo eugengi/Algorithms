349ecf3a720f5d22d861e105358074c065bf1024
Eugene Mwangi <mwangi.em37@gmail.com>
2025-11-13 07:33:21 +0300

    feat: Add challenge impl
    Add implementation to main script to run against tests.

--- easy/strangeCounter/main.py
+++ easy/strangeCounter/main.py
@@ -1,22 +1,49 @@
 """
 || HackerRank
-    Problem: Strange Counter 
+    Problem: Strange Counter
     Level: Easy
     Author: Shafaet
     Implementation: eugengi
 """
 
 # This is main entry point function to complete
-# on the HackerRank platform. 
+# on the HackerRank platform.
 # Rename as per specified
 
-def strange_counter():
-  pass
+import math
+from typing import Iterator
+
+
+def strange_counter(time: int) -> int:
+  for first_value in _generate_cycle_fv_sequence():
+    first_second, last_second = _compute_cycle_time(first_value)
+    if _is_within_range(time, (first_second, last_second,)):
+      relative_position = time - first_second
+      return (first_value - relative_position)
+  return 0
+
+def _generate_cycle_fv_sequence(start: int = 3) -> Iterator[int]:
+  while start < math.pow(10, 12):
+    yield start
+    start *= 2
+
+
+def _compute_cycle_time(first_value: int) -> tuple[int, int]:
+  first_second = first_value - 2
+  last_second = (first_second + first_value) - 1
+  return (first_second, last_second,)
+
+
+def _is_within_range(time: int, cycle: tuple[int, int,]) -> bool:
+  first_time, last_time = cycle
+  return first_time <= time <= last_time
 
 
 def main() -> None:
   # Define your actual argument data here
-  print("The value displayed by the counter is", strange_counter())
+  time = 22
+  result = strange_counter(time)
+  print(f"The value displayed by the counter for time({time}) is {result}")
 
 
 if __name__ == "__main__":
